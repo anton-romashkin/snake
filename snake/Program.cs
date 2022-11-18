264f3060c5c6d1cf84aa5adbbd022316d588f1ac
Anton Romashkin <anton.v.romashkin@gmail.com>
2022-11-18 17:44:10 +0800

    lines

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,46 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            HorizontalLine hLine = new HorizontalLine(1, 21, 10, '+');
+            hLine.Draw();
 
-            List<char> charList = new List<char>();
-            charList.Add('*');
-            charList.Add('#');
-            charList.Add('@');
-
-            foreach(char i in charList)
-            {
-                Console.WriteLine(i);
-            }
-
-            charList.RemoveAt(0);
-
-            foreach (char i in charList)
-            {
-                Console.WriteLine("\n" +i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            var elem = pList[0];
-
-            Console.WriteLine(elem);
+            VerticalLine vLine = new VerticalLine(5, 20, 41, '+');
+            vLine.Draw();
 
             Console.ReadLine();
         }
-    }       
-
+    }
 }
