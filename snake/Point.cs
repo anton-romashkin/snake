fb18f7963a8885ccfd7d025bde68f5208ce83898
Anton Romashkin <anton.v.romashkin@gmail.com>
2022-11-18 17:07:02 +0800

    ram

--- snake/Point.cs
+++ snake/Point.cs
@@ -9,7 +9,13 @@ namespace Snake
 
 		public Point()
 		{
-			Console.WriteLine("создается новая точка");
+		}
+
+		public Point(int _x, int _y, char _sym)
+		{
+			x = _x;
+			y = _y;
+			sym = _sym;
 		}
 
 		public void Draw()
