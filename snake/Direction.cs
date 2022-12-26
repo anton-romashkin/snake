84cc37e5c91a97aa87ae5d8c247c34972510248e
Anton Romashkin <anton.v.romashkin@gmail.com>
2022-12-27 02:29:26 +0800

    added snake class

---
+++ snake/Direction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Snake
+{
+	enum Direction
+	{
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	}
+}
+
--- snake/Point.cs
+++ snake/Point.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Snake
 {
-	public class Point
+	class Point
 	{
 		public int x;
 		public int y;
@@ -18,11 +18,44 @@ namespace Snake
 			sym = _sym;
 		}
 
+		public Point(Point p)
+		{
+			x = p.x;
+			y = p.y;
+			sym = p.sym;
+		}
+
+		public void Move(int offset, Direction direction)
+		{
+			if(direction == Direction.RIGHT)
+			{
+				x = x + offset;
+			}
+			else if(direction == Direction.LEFT)
+			{
+				x = x - offset;
+			}
+			else if(direction == Direction.UP)
+			{
+				y = y + offset;
+			}
+			else if(direction == Direction.DOWN)
+			{
+				y = y - offset;
+			}
+		}
+
+
 		public void Draw()
 		{
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
-	}
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
+        }
+    }
 }
 
